f83c384994ed4749cc400a1bdc2e2be9ab324997
Felix Rao <raof01@gmail.com>
2016-08-01 15:52:31 +0800

    Add cscope-minor-mode to csharp-mode-hook

--- .emacs.d/site-lisp/init-xcscope.el
+++ .emacs.d/site-lisp/init-xcscope.el
@@ -1,4 +1,8 @@
 (require 'xcscope)
-(setq cscope-program "/usr/local/bin/cscope")
+(cond ((string= system-type "darwin")
+       (setq cscope-program "/usr/local/bin/cscope"))
+      ((string= system-type "windows-nt")
+       (setq cscope-program "cscope")))
+(add-hook 'csharp-mode-hook (function cscope-minor-mode))
 (provide 'init-xcscope)
 
