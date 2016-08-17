3c2ea5bdadd630930ce936a5a7f85989fab02ff4
Felix Rao <raof01@gmail.com>
2016-08-17 21:21:06 +0800

    Add js auto completion and move to new place

---
+++ emacs/.emacs.d/site-lisp/init-js3.el
@@ -0,0 +1,13 @@
+;;;; load & configure js2-mode 
+(autoload 'js3-mode "js3-mode" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+
+(setq-default auto-complete-mode t)
+
+(add-hook 'js3-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'tern
+   '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup)))
+ 
+(provide 'init-js3)
