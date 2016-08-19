2c7ac1ae4dee8c3f6abf9c658c4bbfb0d5c7fc49
Felix Rao <raof01@gmail.com>
2016-08-19 23:11:48 +0800

    Update init-faces.el

--- emacs/.emacs.d/site-lisp/init-faces.el
+++ emacs/.emacs.d/site-lisp/init-faces.el
@@ -8,6 +8,11 @@
        (custom-set-faces
 	'(default ((t (:inherit nil :stipple nil :background "SystemWindow" :foreground "SystemWindowText" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "outline" :family "Consolas")))))
        )
-      )
+      ((string-equal system-type "gnu/linux")
+       (custom-set-faces
+	'(cursor ((t (:background "dark red")))))))
+
+(cond ((string-equal system-type "gnu/linux")
+       '(custom-enabled-themes (quote (tango-dark)))))
 (provide 'init-faces)
       
