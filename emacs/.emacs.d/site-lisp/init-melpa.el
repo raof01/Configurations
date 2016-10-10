e2c47aac4eb9e5b30c0c6056f11dea6c3ed19ffe
raof01 <raof01@gmail.com>
2016-10-10 11:43:14 +0800

    Update init-melpa.el

--- emacs/.emacs.d/site-lisp/init-melpa.el
+++ emacs/.emacs.d/site-lisp/init-melpa.el
@@ -7,4 +7,10 @@
   (add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 
+(setq package-archive-priorities
+      '(("melpa-stable" . 20)
+        ("marmalade" . 20)
+        ("gnu" . 10)
+        ("melpa" . 0)))
+
 (provide 'init-melpa)
