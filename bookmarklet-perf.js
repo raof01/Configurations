062d9a9283b72bdda1554491568b2e844817993e
raof01 <raof01@gmail.com>
2020-08-12 15:01:18 +0800

    Create bookmarklet-perf.js

---
+++ bookmarklet-perf.js
@@ -0,0 +1,1 @@
+javascript:(function(){var el=document.createElement('script');el.type='text/javascript';el.src='//i.dell.com/images/global/js/lib/perf-bookmarklet.min.js';document.getElementsByTagName('body')[0].appendChild(el);})();
